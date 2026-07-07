3785f4135f299934c3fc0ff1f28cc07f7d91c9c1
ValeriaPostylyakova <valeria.postylyakova@mail.ru>
2026-07-07 14:35:31 +0300

    настройка cors

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -166,6 +166,7 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://portfolio-frontend-lsnz.onrender.com",
 ]
 
 SECURE_BROWSER_XSS_FILTER = True
