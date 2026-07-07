d82bec447b58d1cb11d38899bbc12648dfc062e7
ValeriaPostylyakova <valeria.postylyakova@mail.ru>
2026-07-07 16:59:33 +0300

    настройка api клиента на фронтенде

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -3,15 +3,17 @@ import i18n from 'i18next';
 
 import { Language } from '../@types/language.types';
 
+const API_URL = import.meta.env.VITE_APP_API_URL;
+
 export const api = axios.create({
-    baseURL: 'http://localhost:8000/api/v1/',
+    baseURL: `${API_URL}/api/v1/`,
     headers: {
         'Content-Type': 'application/json',
     },
 });
 
 api.interceptors.request.use((config) => {
-    const currentLanguage = i18n.language || Language.RU;
+    const currentLanguage = i18n.language || Language.EN;
 
     config.headers['Accept-Language'] = currentLanguage;
     return config;
